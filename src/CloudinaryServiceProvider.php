b6a7de48dce7fd2d74e95cc5f4b8d4fb83143a71
Carlos O Carvalho <quenduk@yahoo.com.br>
2016-02-02 18:48:49 -0200

    first commit

---
+++ src/CloudinaryServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use League\Flysystem\Filesystem;
+use Storage;
+use CarlosOCarvalho\Flysystem\Cloudinary\CloudinaryAdapter;
+
+
+class CloudinaryServiceProvider extends ServiceProvider {
+
+	/**
+	 * Perform post-registration booting of services.
+	 *
+	 * @return void
+	 */
+	public function boot() {
+		Storage::extend('cloudinary', function ($app, $config) {
+			return new Filesystem(new CloudinaryAdapter($config) );
+		});
+	}
+
+	/**
+	 * Register bindings in the container.
+	 *
+	 * @return void
+	 */
+	public function register() {
+		//
+	}
+}
